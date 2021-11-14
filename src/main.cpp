c0a1d020ed0dbd45e289876f98cc810e07fa5ddc
Roman <lderok@gmail.com>
2021-11-14 23:55:32 +0200

    Adds sum and average task solution

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,25 @@
 #include <iostream>
-#include <limits>
-#include "stack.h"
+#include <vector>
 
 int main() {
-    Stack<int>custom_stack;
-    size_t stackSize = custom_stack.size();
-    custom_stack.push(1);
-    std::cout << "stackSize: " << stackSize << '\n';
-    custom_stack.push(2);
-    stackSize = custom_stack.size();
-    std::cout << "stackSize: " << stackSize << '\n';
-    bool isEmptyStack = custom_stack.empty();
-    auto topStackElem = custom_stack.top();
-    custom_stack.pop();
-    stackSize = custom_stack.size();
-    std::cout << "isEmptyStack: " << isEmptyStack << '\n';
-    std::cout << "stackSize: " << stackSize << '\n';
-    std::cout << "topStackElem: " << *topStackElem << '\n';
-    std::cout << "All checks regarding stack was passed" << '\n';
+    std::string string{""};
+    if (!(std::cin >> string)) {
+        std::cin.clear();
+        std::cin.ignore(10000,'\n');
+        std:: cerr << "Something went wrong when read input. Try again" << '\n';
+        return 1;
+    }
+    int int_sum{};
+    for(auto iterator = string.begin(); iterator != string.end(); ++iterator) {
+        if(*iterator < '0' || *iterator > '9') {
+            std:: cerr << "Not number. Please, enter number" << '\n';
+            return 1;
+        }
+        //converting to int from decimal value of char
+        int_sum += *iterator - '0';
+    }
+    std:: cout << "The sum of integers is: " << int_sum << '\n';
+    int average = int_sum/string.length();
+    std:: cout << "An average is: " << average << '\n';
 
-    auto state = true;
-    int intState = static_cast<int>(state);
-    std::cout << std::boolalpha;
-    std::cout << (bool)intState << '\n';
-
-    auto value = std::numeric_limits<int>::max();
-    std::cout << value << '\n';
-
-    double value_1 = 7/2.0;
-    std::cout << value_1 << '\n';
-    double value_2 = 7/2;
-    std::cout << value_2 << '\n';
 }
